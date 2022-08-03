5426be712f190a1da06ede20f8cc1142f38113cf
Your Name <imadmato@gmail.com>
2022-08-03 16:55:33 -0700

    shopify clone

---
+++ src/header.jsx
@@ -0,0 +1,183 @@
+import { Fragment } from 'react'
+import { Popover, Transition } from '@headlessui/react'
+import { MenuIcon, XIcon } from '@heroicons/react/outline'
+import { ChevronRightIcon } from '@heroicons/react/solid';
+import card from './hero.png';
+import logo from './logo.png';
+const navigation = [
+  { name: 'Product', href: '#' },
+  { name: 'Features', href: '#' },
+  { name: 'Marketplace', href: '#' },
+  { name: 'Company', href: '#' },
+]
+
+function Header() {
+  return (
+    <div className="relative overflow-hidden">
+      <Popover as="header" className="relative">
+        <div className=" bg-amber-100 p-6">
+          <nav
+            className="relative max-w-7xl mx-auto flex items-center justify-between px-4 sm:px-6"
+            aria-label="Global"
+          >
+            <div className="flex items-center flex-1">
+              <div className="flex items-center justify-between w-full md:w-auto">
+                <a href="#">
+                  <span className="sr-only">Shopify</span>
+                  <img
+                    className="h-8 w-auto sm:h-10"
+                src={logo}
+                    alt=""
+                  />
+                </a>
+                <div className="-mr-2 flex items-center md:hidden">
+                  <Popover.Button className="bg-gray-900 rounded-md p-2 inline-flex items-center justify-center text-black hover:bg-gray-800 focus:outline-none focus:ring-2 focus-ring-inset focus:ring-white">
+                    <span className="sr-only">Open main menu</span>
+                    <MenuIcon className="h-6 w-6" aria-hidden="true" />
+                  </Popover.Button>
+                </div>
+              </div>
+              <div className="hidden space-x-8 md:flex md:ml-10">
+                {navigation.map((item) => (
+                  <a key={item.name} href={item.href} className="text-base font-medium text-black hover:text-gray-300">
+                    {item.name}
+                  </a>
+                ))}
+              </div>
+            </div>
+            <div className="hidden md:flex md:items-center md:space-x-6">
+              <a href="#" className="text-base font-medium text-black hover:text-gray-800">
+                Log in
+              </a>
+              <a
+                href="#"
+                className="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md text-white bg-green-900 hover:bg-white hover:text-green-900 hover:border-green-900"
+              >
+                Start free
+              </a>
+            </div>
+          </nav>
+        </div>
+
+        <Transition
+          as={Fragment}
+          enter="duration-150 ease-out"
+          enterFrom="opacity-0 scale-95"
+          enterTo="opacity-100 scale-100"
+          leave="duration-100 ease-in"
+          leaveFrom="opacity-100 scale-100"
+          leaveTo="opacity-0 scale-95"
+        >
+          <Popover.Panel focus className="absolute z-10 top-0 inset-x-0 p-2 transition transform origin-top md:hidden">
+            <div className="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
+              <div className="px-5 pt-4 flex items-center justify-between">
+                <div>
+                  <img
+                    className="h-8 w-auto"
+                    src=""
+                    alt=""
+                  />
+                </div>
+                <div className="-mr-2">
+                  <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-600">
+                    <span className="sr-only">Close menu</span>
+                    <XIcon className="h-6 w-6" aria-hidden="true" />
+                  </Popover.Button>
+                </div>
+              </div>
+              <div className="pt-5 pb-6">
+                <div className="px-2 space-y-1">
+                  {navigation.map((item) => (
+                    <a
+                      key={item.name}
+                      href={item.href}
+                      className="block px-3 py-2 rounded-md text-base font-medium text-gray-900 hover:bg-gray-50"
+                    >
+                      {item.name}
+                    </a>
+                  ))}
+                </div>
+                <div className="mt-6 px-5">
+                  <a
+                    href="#"
+                    className="block text-center w-full py-3 px-4 rounded-md shadow text-white bg-green-900 hover:bg-white hover:text-green-900 hover:border-green-900"
+                  >
+                    Start free
+                  </a>
+                </div>
+                <div className="mt-6 px-5">
+                  <p className="text-center text-base font-medium text-gray-500">
+                    Existing customer?{' '}
+                    <a href="#" className="text-gray-900 hover:underline">
+                      Login
+                    </a>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </Popover.Panel>
+        </Transition>
+      </Popover>
+
+      <main>
+        <div className=" bg-amber-100 -mb-16 sm:-mb-48 lg:m-0 lg:overflow-hidden">
+          <div className="mx-auto max-w-7xl lg:px-4">
+            <div className="lg:grid lg:grid-cols-2 lg:gap-8">
+              <div className="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 sm:text-center lg:px-0 lg:text-left lg:flex lg:items-center">
+                <div className="lg:py-24">
+                  
+                  <h1 className="text-4xl tracking-tight text-bold text-green-900 sm:mt-5 sm:text-6xl lg:mt-6 xl:text-8xl">
+                    <span className="block">The platform</span>
+                    <span className="block text-green-900"> commerce is built on.</span>
+                  </h1>
+                  <p className="mt-3 text-base text-gray-600 sm:mt-5 sm:text-xl lg:text-lg xl:text-3xl">
+                  Millions of the world's most successful brands trust 
+                  Shopify to sell, ship and process payments anywhere.
+                  </p>
+                  <div className="mt-10 sm:mt-12">
+                    <form action="#" className="sm:max-w-xl sm:mx-auto lg:mx-0">
+                      <div className="sm:flex">
+                        <div className="min-w-0 flex-2">
+                        <button
+                            type="submit"
+                            className="block w-full py-3 px-4 rounded-md shadow bg-green-900 text-amber-100 font-medium
+                             hover:bg-amber-100 hover:text-green-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-300 focus:ring-offset-gray-900"
+                          >
+                            Start free 
+                          </button>
+                        </div>
+                        <div className="mt-3 sm:mt-0 sm:ml-3">
+                          <button
+                            type="submit"
+                            className="block w-full py-3 px-4 rounded-md shadow bg-amber-100 text-green-900
+                            border-2 border-green-900 font-medium hover:bg-green-900 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-300 focus:ring-offset-gray-900"
+                          >
+                            Start free
+                          </button>
+                        </div>
+                      </div>
+                     
+                    </form>
+                  </div>
+                </div>
+              </div>
+              <div className="mt-12 -mb-16 sm:-mb-48 lg:m-0 lg:relative">
+                <div className="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 lg:max-w-none lg:px-0">
+                  {/* Illustration taken from Lucid Illustrations: https://lucid.pixsellz.io/ */}
+                  <img
+                    className="w-full lg:absolute lg:inset-y-0 -pr-16 lg:left-0 lg:h-full lg:w-300 lg:max-w-none"
+                    src={card}
+                    alt=""
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* More main page content here... */}
+      </main>
+    </div>
+  )
+}
+export default Header;
