e5095ae19468d7347691c4e863d1f872ffc1f19f
Your Name <imadmato@gmail.com>
2022-08-09 01:12:08 -0700

    store front added

--- src/App.js
+++ src/App.js
@@ -5,11 +5,15 @@ import Shopify from './shopify';
 import Storefront from './storefront';
 import Pwa from './pwa';
 import Customers from './customers';
+import Blog from './blog';
+import Footer from './footer';
 
 function App() {
   return (
     <div>
      <Header />
+     <Storefront />
+     <Pwa />
     </div>
   );
 }
